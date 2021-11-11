fb25e7a8f72d1e922e8dad8bb3dbe70882bf85a9
Wassim <wassim.zouitene@efrei.net>
2021-11-11 22:24:32 +0100

    feat def top

--- Report.scala
+++ Report.scala
@@ -7,25 +7,38 @@ object Report {
 
     val countryAndNbAirports: List[(String, Int)] = airportsData.map(_.split(",")(8)).groupBy(identity).view.mapValues { _.size }.toList
     
+
+    def topCountries: Unit = {
+
+        //Liste Triée de façon croissante ou décroissante
+        
+        val listeCountriesTrieeAsc: List[(String, Int)] = countryAndNbAirports.sortWith(_._2 < _._2)
+        val listeCountriesrieeDesc: List[(String, Int)] = countryAndNbAirports.sortWith(-_._2 < -_._2)
+
+
+        //Top 10 
+        
+        val topTenLowestCountries: List[(String, Int)] = listeCountriesTrieeAsc.map { case (code, nb) => (getCountryWithCodeOrCountry(code), nb) }.take(10)
+        val topTenHighestCountries: List[(String, Int)] = listeCountriesrieeDesc.map { case (code, nb) => (getCountryWithCodeOrCountry(code), nb) }.take(10)
+
+        println("Top 10 countries with highest number of airports :" + '\n')
+        topTenHighestCountries.foreach(println)
+        println('\n')
+        println("Top 10 countries with lowest number of airports: " + '\n')
+        topTenLowestCountries.foreach(println)
+ 
+    }   
+    
+    val runwaysType: Map[String, String] = runwaysData.map { x => (x.split(",")(2), x.split(",")(5)) }.toMap
+
+
+
     val runwaysLatitude: List[String] = runwaysData.map(x => Try { x.split(",")(8) }).groupBy(identity)
     .mapValues { _.size }.toList
 
-    //Liste Triée de façon croissante ou décroissante
-    
-    val listeCountriesTrieeAsc: List[(String, Int)] = countryAndNbAirports.sortWith(_._2 < _._2)
-    val listeCountriesrieeDesc: List[(String, Int)] = countryAndNbAirports.sortWith(-_._2 < -_._2)
-
     val listeRunways: List[String] = runwaysLatitude.sortWith(-_._2 < -_._2)
 
-    //Top 10 
-    
-    val topTenLowestCountries: List[(String, Int)] = listeCountriesTrieeAsc.map { case (code, nb) => (getCountryWithCodeOrCountry(code), nb) }.take(10)
-    val topTenHighestCountries: List[(String, Int)] = listeCountriesrieeDesc.map { case (code, nb) => (getCountryWithCodeOrCountry(code), nb) }.take(10)
-
-    val topTenRunways: List[String] = runwaysLatitude.take(10)    
-    
-    
-    val runwaysType: Map[String, String] = runwaysData.map { x => (x.split(",")(2), x.split(",")(5)) }.toMap
+    val topTenRunways: List[String] = runwaysLatitude.take(10)       
 
 
 
