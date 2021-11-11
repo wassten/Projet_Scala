2ae83b5d251929511361ac8c4f8db24f6e2cc85c
Hatim Manowaraly <hatimmanowaraly@MacBook-Air-de-Hatim.local>
2021-11-11 13:55:08 +0100

    rename

--- airport.scala
+++ airport.scala
@@ -5,13 +5,9 @@ import java.io.File
 case class airportsAndRunways(country: String, airportName: String, airportCode: String, runways: List[String] = List[String]())
 
 object Airport {
-    
-    val airportsFile: String = "./public/airports.csv"
-    val countriesFile: String = "./public/countries.csv"
-    val runwaysFile: String = "./public/runways.csv"
 
-    val airportsData = Source.fromFile(new File(airportsFile), "ISO-8859-1").getLines.drop(1)
-    val countriesData = Source.fromFile(new File(countriesFile), "ISO-8859-1").getLines.drop(1)
-    val runwaysData = Source.fromFile(new File(runwaysFile), "ISO-8859-1").getLines.drop(1)
+    val airportsData = Source.fromFile(new File("./public/airports.csv"), "ISO-8859-1").getLines.drop(1)
+    val countriesData = Source.fromFile(new File("./public/countries.csv"), "ISO-8859-1").getLines.drop(1)
+    val runwaysData = Source.fromFile(new File("./public/runways.csv"), "ISO-8859-1").getLines.drop(1)
 
 }
