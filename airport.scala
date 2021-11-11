34a1428d3366823ff0b0a7b0721a785aaf308dc6
Hatim Manowaraly <hatimmanowaraly@MacBook-Air-de-Hatim.local>
2021-11-11 14:03:18 +0100

    file

--- airport.scala
+++ airport.scala
@@ -6,8 +6,8 @@ case class airportsAndRunways(country: String, airportName: String, airportCode:
 
 object Airport {
 
-    val airportsData = Source.fromFile(new File("./public/airports.csv"), "ISO-8859-1").getLines.drop(1)
-    val countriesData = Source.fromFile(new File("./public/countries.csv"), "ISO-8859-1").getLines.drop(1)
-    val runwaysData = Source.fromFile(new File("./public/runways.csv"), "ISO-8859-1").getLines.drop(1)
+    val airportsData = Source.fromFile(new File("./data/airports.csv"), "ISO-8859-1").getLines.drop(1)
+    val countriesData = Source.fromFile(new File("./data/countries.csv"), "ISO-8859-1").getLines.drop(1)
+    val runwaysData = Source.fromFile(new File("./data/runways.csv"), "ISO-8859-1").getLines.drop(1)
 
 }
