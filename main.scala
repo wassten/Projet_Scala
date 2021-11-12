42f0a55cb44268feae21317977062be57b99a440
Hatim Manowaraly <hatim.mano@hotmail.fr>
2021-11-12 10:34:53 +0100

    main

--- main.scala
+++ main.scala
@@ -1,3 +1,10 @@
 object Main extends App {
-  println("Hello, World!")
+   println("Quel est votre choix ? \n1 - Report \n2 - Query")
+  val choix = scala.io.StdIn.readLine()
+
+
+  def program(choix : String): Unit = choix match {
+    case "1" => topCountries()
+    case "2" => 
+
 }
