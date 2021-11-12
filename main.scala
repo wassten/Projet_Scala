051fa4b7429dc899b1f5a040ef8f72f3dbc21725
Hatim Manowaraly <hatim.mano@hotmail.fr>
2021-11-12 11:20:32 +0100

    push

--- main.scala
+++ main.scala
@@ -1,11 +1,17 @@
 object Main extends App {
-println("Quel est votre choix ? \nReport (R) \nQuery (Q)")
-  val choix = scala.io.StdIn.readLine()
+   println("Quel est votre choix ? \n1 - Report \n2 - Query")
+  val choix1 = scala.io.StdIn.readLine()
 
 
   def program(choix : String): Unit = choix match {
-    case "R" => println(topCountries())
-    case "Q" => println("ERROR 418 : I'm a teapot")
-  }
+    case "1" => println("Que voulez vous savoir ? \n1 - Top 10 countries \n2 - Type Runways \n2 Top 10 runway latitude")
+      val choix2 = scala.io.StdIn.readLine()
+      sous_program(choix2)
+    case "2" => 
 
+  def sous_program(choix : String): Unit = choix match {
+    case "1" => println(topCountries())
+    case "2" => 
+    case "3" => println()
+  }
 }
